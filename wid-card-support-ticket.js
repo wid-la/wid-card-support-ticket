cc2d47e371ed0222b73f91a4b53cf8a1807fc091
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-07 15:09:17 -0700

    Improves styling.

--- wid-card-support-ticket.js
+++ wid-card-support-ticket.js
@@ -6,7 +6,51 @@
 
     is: 'wid-card-support-ticket',
 
-    properties: {}
+    behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior],
+
+    properties: {
+      /**
+       * Name
+       * @type {String}
+       */
+      name: {
+        type: String,
+        value: 'Support'
+      },
+
+      /**
+       * icon
+       * @type {String}
+       */
+      icon: {
+        type: String
+      },
+
+      /**
+       * tickets
+       * @type {Object}
+       */
+      tickets: {
+        type: Object
+      }
+
+    },
+
+    _iconPath: function _iconPath(value) {
+      return '/images/cards/' + value;
+    },
+
+    _ticketId: function _ticketId(value) {
+      return '#' + value;
+    },
+
+    _rowClass: function _rowClass(index) {
+      var odd = '';
+      if (index % 2 === 0) {
+        odd = 'odd';
+      }
+      return odd + ' flex layout horizontal center-center';
+    }
 
   });
 })();
